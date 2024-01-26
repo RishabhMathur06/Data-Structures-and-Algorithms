4ebdf5cc4c5ef328434bd1141e1f47576c531076
Rishabh Mathur <107912515+RishabhMathur06@users.noreply.github.com>
2024-01-26 23:58:09 +0530

    26 Jan, 2024

---
+++ GeeksforGeeks POTD/Fractional_Knapsack.py
@@ -0,0 +1,59 @@
+#User function Template for python3
+
+class Item:
+    def __init__(self,val,w):
+        self.value = val
+        self.weight = w
+        
+class Solution:    
+    #Function to get the maximum total value in the knapsack.
+    def fractionalknapsack(self, W,arr,n):
+        
+        # code here
+        sorted_items = sorted([[item.value/item.weight, item.value, item.weight] for item in arr], reverse=True)
+        
+        total_value = 0
+        current = 0
+        
+        while current < n:
+            if sorted_items[current][2] <= W:
+                total_value += sorted_items[current][1]
+                W -= sorted_items[current][2]
+                
+            else:
+                total_value += (W * sorted_items[current][0])
+                break
+                
+            current += 1
+            
+        return total_value
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+class Item:
+    def __init__(self,val,w):
+        self.value = val
+        self.weight = w
+        
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n,W = map(int,input().strip().split())
+        info = list(map(int,input().strip().split()))
+        arr = [Item(0,0) for i in range(n)]
+        for i in range(n):
+            arr[i].value = info[2*i]
+            arr[i].weight = info[2*i+1]
+            
+        ob=Solution()
+        print("%.6f" %ob.fractionalknapsack(W,arr,n))
+
+# } Driver Code Ends
