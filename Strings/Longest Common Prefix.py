0654714bfd3bc6e2016bbd9b0fd847e5a620ca4d
Rishabh Mathur <107912515+RishabhMathur06@users.noreply.github.com>
2024-02-15 12:17:06 +0530

    Third Commit

---
+++ Strings/Longest Common Prefix.py
@@ -0,0 +1,27 @@
+class Solution:
+    def lcp(self, arr):
+        n = len(arr)
+
+        # If no element in array, return empty string.
+        if (n==0):
+            return ""
+        
+        # If only single element is present in array then it's LCP will be arr[0].
+        if (n==1):
+            return arr[0]
+            
+        # Sort the array in lexicographically order. 
+        arr.sort()
+
+        string = ""
+        string = " ".join(arr[0])
+
+        return string
+        
+if __name__ == "__main__":
+    arr = ["geeksforgeeks", "geeks", "geek", "geezer"]
+
+    ob = Solution()
+
+    ans = ob.lcp(arr)
+    print(ans)
